eba2f3a7824cee33ee4a4e2f591b8c78f88be871
muskanalirizvi <98577455+muskanalirizvi@users.noreply.github.com>
2023-08-15 01:41:27 -0700

    Update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,18 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Tech enthusiast and software engineering student, exploring innovation and embracing real-world challenges through code.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Communication list",
+  "Familiar with C++,java, sql and python",
+  "Good management",
+  "Fluent in English",
+  "Video editing",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
